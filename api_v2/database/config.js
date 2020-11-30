d56726c5968d00f10d20e7fab5c1abc8ba0efd1c
Rob Conery <rob@conery.io>
2020-11-30 09:23:45 -0800

    An idea for the db config

--- api_v2/database/config.js
+++ api_v2/database/config.js
@@ -1,20 +1,36 @@
 const { database } = require('swa-api');
 
+function canEdit(user, data){
+    return user.id === data.user.id || user.roles.indexOf("admin") > -1;
+}
+
 module.exports = database.config({
     collections: {
         "todos": {
-            permissions: [
-                {
-                    operations: [
-                        "findDocuments",
-                        "insertDocument",
-                        "replaceDocument",
-                        "deleteDocument"
-                    ],
-                    allowedRoles: [ "authenticated" ],
-                    restrictDocsByUser: true
-                }
-            ],
+            create: canEdit,
+            update: canEdit,
+            delete(args){
+                return canEdit(args) && !args.data.locked;
+            },
+            get({auth, id}){
+                return auth !== null;
+            },
+            list({auth}){
+                return true;
+            },
+            authenticated: true, //as an option
+            // permissions: [
+            //     {
+            //         operations: [
+            //             "findDocuments",
+            //             "insertDocument",
+            //             "replaceDocument",
+            //             "deleteDocument"
+            //         ],
+            //         allowedRoles: [ "authenticated" ],
+            //         restrictDocsByUser: true
+            //     }
+            // ],
             notifyOnChange: true
         }
     }
